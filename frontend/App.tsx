54ee268173b5a47b94a1e92eaf1fc022d8e23b02
Marcus Hellberg <marcus@vaadin.com>
2023-04-24 14:55:45 -0700

    Add completed code, readme

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -5,17 +5,21 @@ import {PersonEndpoint} from "Frontend/generated/endpoints.js";
 import {useEffect, useState} from "react";
 
 export default function App() {
-  const [people, setPeople] = useState<Person[]>([]);
 
-  useEffect(() => {
-    PersonEndpoint.findAll().then(setPeople);
-  }, []);
+  async function dataProvider(
+    params: GridDataProviderParams<Person>,
+    callback: GridDataProviderCallback<Person>
+  ) {
+    const res = await PersonEndpoint.getPeople(params.page, params.pageSize);
+
+    callback(res.items, res.totalCount);
+  }
 
   return (
     <div className="flex flex-col gap-m p-m">
       <h1>Lazy-load / infinite scroll data grid</h1>
 
-      <Grid className="flex-grow" items={people}>
+      <Grid className="flex-grow" dataProvider={dataProvider}>
         <GridColumn path="firstName" />
         <GridColumn path="lastName" />
         <GridColumn path="email" autoWidth/>
--- src/main/java/com/example/application/endpoints/PersonEndpoint.java
+++ src/main/java/com/example/application/endpoints/PersonEndpoint.java
@@ -4,6 +4,7 @@ import com.example.application.data.Person;
 import com.example.application.service.PersonRepository;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import dev.hilla.Endpoint;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 
@@ -13,6 +14,13 @@ public class PersonEndpoint {
 
     private final PersonRepository personRepository;
 
+    public record PageResponse(
+            List<Person> items,
+            long totalCount
+    ) {
+    }
+
+
     public PersonEndpoint(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
@@ -21,4 +29,9 @@ public class PersonEndpoint {
         return personRepository.findAll();
     }
 
+    public PageResponse getPeople(int page, int pageSize) {
+        var res = personRepository.findAll(PageRequest.of(page, pageSize));
+
+        return new PageResponse(res.getContent(), res.getTotalElements());
+    }
 }
